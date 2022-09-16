dbed788c389cc061f6117e479f9219f62c839b34
tuliofergulha <tfergulha@gmail.com>
2022-09-16 10:06:12 -0300

    fix: Fix test wich was not passing with soma function

--- math_test.go
+++ math_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSoma(t *testing.T) {
 
-	total := Soma(15, 15)
+	total := soma(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
